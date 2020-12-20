1475652733726edc66724e91527ac798ba0d13d6
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2020-12-20 15:51:19 -0300

    criando o modulo shared, o service httpBuilder e o component tabela

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabelaComponent } from './tabela/tabela.component';
+
+@NgModule({
+  declarations: [
+    TabelaComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    TabelaComponent
+  ]
+})
+export class SharedModule { }
