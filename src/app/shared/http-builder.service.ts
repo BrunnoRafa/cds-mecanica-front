16fd9a344768b4759f538b764db9a6a8c743c7bc
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2021-01-24 16:05:01 -0300

    adicionando configuração de headers no httpBuilder

--- src/app/shared/http-builder.service.ts
+++ src/app/shared/http-builder.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+const headers = new HttpHeaders();
+headers.set('Content-Type', 'application/json');
 
 @Injectable({ providedIn: 'root' })
 export class HttpBuilder {
@@ -8,7 +11,18 @@ export class HttpBuilder {
   constructor(private httpClient: HttpClient) { }
 
   get<T>(url: string): Observable<T> {
-    return this.httpClient.get<T>(url);
+    return this.httpClient.get<T>(url, { headers });
   }
 
+  post<T>(url: string, body: T): Observable<T> {
+    return this.httpClient.post<T>(url, body, { headers });
+  }
+
+  put<T>(url: string, body: T): Observable<T> {
+    return this.httpClient.put<T>(url, body, { headers });
+  }
+
+  delete<T>(url: string): Observable<T> {
+    return this.httpClient.delete<T>(url, { headers });
+  }
 }
